e403a1cc5879920b4b6ef03115267f581671d271
Varbris <92345143+Varbris@users.noreply.github.com>
2025-01-21 14:13:19 +0700

    Add algorithm to exercise 2 and test it

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function (text, num) {
+  let repeatedString = "";
+  if (num < 0) {
+    repeatedString = "ERROR";
+  } else {
+    for (let i = 0; i < num; i++) {
+      if (num == 0) {
+        break;
+      }
+      repeatedString += text;
+    }
+  }
 
+  return repeatedString;
 };
 
 // Do not edit below this line
