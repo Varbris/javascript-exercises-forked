5038be42634a1ff405e2cb3f1d573907f665bf19
Varbris <92345143+Varbris@users.noreply.github.com>
2025-01-21 15:14:27 +0700

    Add algorithm to exercise 3 and test it

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function (text) {
+  let arrText = text.split("");
+  let arrTextReversed = [];
+  let result = "";
 
+  for (let i = 0; i < arrText.length; i++) {
+    let j = arrText.length - 1;
+    arrTextReversed[i] = arrText[j - i];
+    result += arrTextReversed[i];
+  }
+  return result;
 };
 
 // Do not edit below this line
