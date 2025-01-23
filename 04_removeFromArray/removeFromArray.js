01466576231b07021d3d56da31e4b3d646c9f8b0
Varbris <92345143+Varbris@users.noreply.github.com>
2025-01-23 13:46:59 +0700

    Add algorithm to exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (arrayVar, ...rest) {
+  const newArray = [];
+  arrayVar.forEach((element) => {
+    if (!rest.includes(element)) {
+      newArray.push(element);
+    }
+  });
+
+  console.log(newArray);
+  return newArray;
 };
 
 // Do not edit below this line
