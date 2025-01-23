3e06b2cd15cb8b65bc09d0456963abb315cc4d35
Varbris <92345143+Varbris@users.noreply.github.com>
2025-01-23 14:06:56 +0700

    Add algorithm to exercise 05 and test it

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  let result = 0;
+  if (Math.sign(num1) === -1 || Math.sign(num2) === -1) {
+    return "ERROR";
+  } else if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+    return "ERROR";
+  }
 
+  if (num1 < num2) {
+    for (let i = num1; i <= num2; i++) {
+      result += i;
+    }
+  } else {
+    for (let i = num2; i <= num1; i++) {
+      result += i;
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
