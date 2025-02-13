4ba18aa281d4f1d00d6af77922e397f8cfeb6f0d
Varbris <92345143+Varbris@users.noreply.github.com>
2025-02-13 11:47:22 +0700

    Fix exercies 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function (var1, var2) {
+  return var1 + var2;
 };
 
-const subtract = function() {
-	
+const subtract = function (var1, var2) {
+  return var1 - var2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let result = 0;
+  if (arr.length !== 0) {
+    arr.forEach((element) => {
+      result += element;
+    });
+  } else {
+    return result;
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let result = 0;
+  arr.forEach(function (element) {
+    if (result === 0) {
+      result = element;
+    } else {
+      result *= element;
+    }
+  });
+  return result;
 };
 
-const power = function() {
-	
+const power = function (var1, var2) {
+  return Math.pow(var1, var2);
 };
 
-const factorial = function() {
-	
+const factorial = function (var1) {
+  let result = 1;
+  for (let i = var1; i > 0; i--) {
+    result *= i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
@@ -29,5 +50,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
