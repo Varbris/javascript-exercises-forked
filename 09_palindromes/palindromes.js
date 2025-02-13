8d74f949db4435e4f4010ba6114f82ffaab5b5e0
Varbris <92345143+Varbris@users.noreply.github.com>
2025-02-13 17:35:31 +0700

    Fix exercise 09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (text) {
+  const textLowered = text.toLowerCase();
+  const arr = textLowered.split("");
+  const filterArrFromUnique = arr.filter(function (character) {
+    const alphabetAndNumber = "abcdefghijklmnopqrstuvwxyz0123456789";
+    return alphabetAndNumber.includes(character);
+  });
+  const cleanText = filterArrFromUnique.join("");
 
+  //test the text is can be reversed
+  const reversedArr = filterArrFromUnique.reverse().join("");
+  return reversedArr === cleanText;
 };
 
 // Do not edit below this line
