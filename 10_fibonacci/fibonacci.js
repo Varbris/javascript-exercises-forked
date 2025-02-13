db6f36fbb52ae1d23964eead96a9da356dc788f1
Varbris <92345143+Varbris@users.noreply.github.com>
2025-02-13 18:17:26 +0700

    Fix excercise 10 fibonacci
    personal note:
    learn more about reccursion

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  const cleanInput = parseInt(num);
 
+  if (cleanInput === 0) {
+    return 0;
+  } else if (cleanInput === 1) {
+    return 1;
+  } else if (cleanInput < 0) {
+    return "OOPS";
+  }
+  return fibonacci(cleanInput - 1) + fibonacci(cleanInput - 2);
 };
 
 // Do not edit below this line
